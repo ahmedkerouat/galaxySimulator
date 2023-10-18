f26d3e16099587061838528b2f7ed5fa04cd83bb
ahmedkerouat <105589873+ahmedkerouat@users.noreply.github.com>
2023-10-18 05:32:22 +0100

    Update computeShader.glsl

--- source/shaders/computeShader.glsl
+++ source/shaders/computeShader.glsl
@@ -1,6 +1,6 @@
 #version 430
 
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
 // Define the buffer blocks for sphere positions and velocities
 layout(std430, binding = 0) buffer SpherePositions {
@@ -15,14 +15,14 @@ const float epsilon = 0.001;
 
 void main() {
     uint sphereIndex = gl_GlobalInvocationID.x;
+    uint sphereCount = spherePositions.length();
 
-    // Calculate gravitational forces
-    vec3 acceleration = vec3(0.0);
     vec3 currentPosition = spherePositions[sphereIndex];
+    vec3 acceleration = vec3(0.0);
 
-    for (uint i = 0; i < spherePositions.length(); i++) {
-        if (i != sphereIndex) {
-            vec3 otherPosition = spherePositions[i];
+    for (uint j = 0; j < sphereCount; j++) {
+        if (j != sphereIndex) {
+            vec3 otherPosition = spherePositions[j];
             vec3 direction = otherPosition - currentPosition;
             float distance = length(direction);
 
