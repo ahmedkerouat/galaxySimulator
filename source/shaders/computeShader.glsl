3d46e2d35e44578f42ce577a0b888c97d3d273a9
ahmedkerouat <105589873+ahmedkerouat@users.noreply.github.com>
2023-10-24 12:32:13 +0100

    add effects of black hole, dark matter and dark energy

--- source/shaders/computeShader.glsl
+++ source/shaders/computeShader.glsl
@@ -12,6 +12,12 @@ layout(std430, binding = 1) buffer SphereVelocities {
 };
 
 const float epsilon = 0.001;
+const float darkMatterMass = 10.0; // Mass of dark matter
+const float darkEnergyAcceleration = 0.0001; // Acceleration due to dark energy
+const float deltaTime = 0.001;
+
+const vec3 supermassiveBlackHolePosition = vec3(0.0, 0.0, 0.0);
+const float supermassiveBlackHoleMass = 1000.0; // Mass of the supermassive black hole
 
 void main() {
     uint sphereIndex = gl_GlobalInvocationID.x;
@@ -20,25 +26,38 @@ void main() {
     vec3 currentPosition = spherePositions[sphereIndex];
     vec3 acceleration = vec3(0.0);
 
+    // Calculate the force due to the supermassive black hole
+    vec3 blackHoleDirection = supermassiveBlackHolePosition - currentPosition;
+    float blackHoleDistance = length(blackHoleDirection);
+    float forceMagnitudeBlackHole = supermassiveBlackHoleMass / (blackHoleDistance * blackHoleDistance + epsilon);
+    vec3 forceBlackHole = forceMagnitudeBlackHole * blackHoleDirection;
+
     for (uint j = 0; j < sphereCount; j++) {
         if (j != sphereIndex) {
             vec3 otherPosition = spherePositions[j];
             vec3 direction = otherPosition - currentPosition;
             float distance = length(direction);
 
-            // Calculate gravitational force using a simple formula
-            float forceMagnitude = 1.0 / (distance * distance + epsilon);
-            vec3 force = forceMagnitude * direction;
+            // Calculate gravitational force due to dark matter
+            float forceMagnitudeDarkMatter = darkMatterMass / (distance * distance + epsilon);
+            vec3 forceDarkMatter = forceMagnitudeDarkMatter * direction;
 
             // Accumulate forces
-            acceleration += force;
+            acceleration += forceDarkMatter;
         }
     }
 
+    // Add the effect of dark energy
+    acceleration += darkEnergyAcceleration;
+
     // Update velocity and position
-    float deltaTime = 0.001;
     vec3 velocity = sphereVelocities[sphereIndex];
     velocity += acceleration * deltaTime;
+
+    // For orbiting motion, set the velocity perpendicular to the black hole's position
+    vec3 perpendicularVelocity = cross(blackHoleDirection, vec3(0.0, 1.0, 0.0));
+    velocity += perpendicularVelocity * deltaTime;
+
     currentPosition += velocity * deltaTime;
 
     // Update the position and velocity of the sphere
